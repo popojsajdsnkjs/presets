06685067480eda2eb93b9cb42c5a73adbaad84f6
popojsajdsnkjs <karol.gacek17@wp.pl>
2025-03-20 09:48:13 +0100

    js i preset box fix

--- assets/script.js
+++ assets/script.js
@@ -67,7 +67,88 @@ const code = {
             <div id="load"></div>
         </div>
     </div>
-    */`
+    */`,
+    "header-banner":`/* potrzeba by dobrze wyglądało
+        *{
+            margin:0;
+            box-sizing: border-box;
+        }*/
+        :root{
+        /*adjust to your liking*/
+            --text-color: white;
+            --link-text-color: black;
+
+            --banner-height: 170px;
+            --banner-background-color: gray;
+            --banner-shadow-color: rgba(0,0,0,0.9);
+
+            --shadow-blur: 10px;
+
+            --title-background: rgba(0,0,0,0);
+            --menu-background: rgba(255,255,255,0.1);
+            --menu-link-background: rgba(255,255,255,0.7);
+            --menu-hover-link-background: rgba(255,255,255,1);
+
+            --link-border: 15px;
+            --link-font-size-mod: 5px; /*wartość bęzie odjęta od wielkości czcionki*/ 
+            --font-adjuster: 100vw * 0.001; /*odejmuje wartość*/
+        }
+
+        header{
+            user-select: none;
+            height: var(--height);
+            background-color: var(--banner-background-color);
+            box-shadow: 0px 5px var(--shadow-blur) var(--banner-shadow-color);
+            margin-bottom: 10px;
+            width: 100vw;
+        }
+        
+        header *{
+            transition: 300ms;
+            color: var(--text-color);
+        }
+
+        header h2{
+            height: calc(var(--banner-height) / 2);
+            line-height: calc(var(--banner-height) / 2);
+            text-align: center;
+            font-size: calc(var(--banner-height) / 2 * 0.8);
+            background-color: var(--title-background);
+        }
+
+        header #menu{
+            height: calc(var(--banner-height) / 2);
+            line-height: calc(var(--banner-height) / 2);
+            text-align: center;
+            font-size: calc(var(--banner-height) / 2 * 0.4 - var(--link-font-size-mod) - var(--font-adjuster));
+            background-color: var(--menu-background);
+        }
+
+        header #menu a{
+            color:var(--link-text-color);
+            text-decoration: none;
+            padding: 10px;
+            margin: 0px 10px 0px 10px;
+            background-color: var(--menu-link-background);
+            border-radius:var(--link-border);
+        }
+
+        header #menu a:hover{
+            background-color: var(--menu-hover-link-background);
+        }
+    /* jak użyć
+    </style>
+    <header>
+        <h2>Tytuł</h2>
+        <div id="menu">
+            <p>
+                <a href="banner.html" class="menu-link">test1</a>
+                <a href="banner.html" class="menu-link">test2</a>
+            </p>
+        </div>
+    </header>
+    */
+    `
 }
 
 function copyToClipboard(text) {
@@ -79,3 +160,4 @@ function copyToClipboard(text) {
   }
 
 document.getElementById('loading-bar-animation-button').onclick = copyToClipboard(code["loading-bar-animation"])
+document.getElementById('header-banner-button').onclick = copyToClipboard(code["header-banner"])
